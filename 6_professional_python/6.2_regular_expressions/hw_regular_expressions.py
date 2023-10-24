2e3728cca1b036eb24dc79283a1f93d73a77cd8f
Dmitrii Kovalev <sirpen9uin@gmail.com>
2023-10-24 17:40:56 +0300

    Task 1. Regular expressions ver.0.1

--- 6_professional_python/6.2_regular_expressions/hw_regular_expressions.py
+++ 6_professional_python/6.2_regular_expressions/hw_regular_expressions.py
@@ -9,9 +9,8 @@ with open("phonebook_raw.csv", encoding="utf-8") as f:
 new_contact_list = []
 # pp(contacts_list)
 for contact in contacts_list:
-    full_name = ' '.join(contact)
+    full_name = ' '.join(contact[:3])
     contact_parts = full_name.split(' ')
     contact_parts = [info for info in contact_parts if len(info) > 0]
     new_contact_list.append(contact_parts)
 print(new_contact_list)
-
