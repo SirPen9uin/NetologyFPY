e36e4a79e781601023e9dcb5e75966bded0dd737
Dmitrii Kovalev <sirpen9uin@gmail.com>
2023-10-24 16:54:43 +0300

    Task 1. Regular expressions

---
+++ 6_professional_python/6.2_regular_expressions/hw_regular_expressions.py
@@ -0,0 +1,17 @@
+import csv
+from pprint import pprint as pp
+import re
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+
+new_contact_list = []
+# pp(contacts_list)
+for contact in contacts_list:
+    full_name = ' '.join(contact)
+    contact_parts = full_name.split(' ')
+    contact_parts = [info for info in contact_parts if len(info) > 0]
+    new_contact_list.append(contact_parts)
+print(new_contact_list)
+
