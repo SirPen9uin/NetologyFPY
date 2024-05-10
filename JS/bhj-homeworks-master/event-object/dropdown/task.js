ff6bd8a3793ece3aacbb46b15c9c2c476030ff46
Dmitrii Kovalev <sirpen9uin@gmail.com>
2024-05-10 13:33:36 +0300

    dropdown Done

---
+++ JS/bhj-homeworks-master/event-object/dropdown/task.js
@@ -0,0 +1,13 @@
+let dropdownValue = document.querySelector('.dropdown__value');
+let dropdownList = document.querySelector('.dropdown__list');
+
+dropdownValue.addEventListener('click', () => {
+        dropdownList.classList.toggle('dropdown__list_active');
+    });
+
+dropdownList.addEventListener('click', (event) => {
+    event.preventDefault();
+    const selectedItem = event.target.textContent;
+    dropdownValue.textContent = selectedItem;
+    dropdownList.classList.remove('dropdown__list_active');
+    });
