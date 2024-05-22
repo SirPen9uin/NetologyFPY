8843e1822cfcb993517a3cdd0ec7ce0bd73242ff
Dmitrii Kovalev <sirpen9uin@gmail.com>
2024-05-22 14:16:16 +0300

    book reader extras done

--- JS/bhj-homeworks-master/dom/ads/task.js
+++ JS/bhj-homeworks-master/dom/ads/task.js
@@ -2,7 +2,6 @@ const ads = document.querySelectorAll('.rotator__case');
 
 function rotate() {
     for (let i = 0; i < ads.length; i++) {
-        let speed = ads[i].getAttribute('data-speed');
         if (i === ads.length - 1) {
             ads[ads.length - 1].classList.remove('rotator__case_active');
             ads[0].classList.add('rotator__case_active');
@@ -14,5 +13,36 @@ function rotate() {
         }
     }
 }
+// Повышенный уровень сложности
 
+// function getSpeed(mass, index) {
+//     return mass[index].getAttribute('data-speed');
+// }
+
+// function getIndex(mass) {
+//     for (let i = 0; i < mass.length; i++) {
+//         if (i === 0) {
+//             return 0;
+//         }
+//         if (i === mass.length - 1) {
+//             return 0;
+//         } else {
+//             return i + 1;
+//         }
+//     }
+// }
+
+// function rotate(mass, index) {
+//     if (i === mass.length - 1) {
+//         mass[index - 1].classList.remove('rotator__case_active');
+//         mass[0].classList.add('rotator__case_active');
+//     }
+//     if (mass[index].classList.contains('rotator__case_active')) {
+//         mass[index].classList.remove('rotator__case_active');
+//         mass[index + 1].classList.add('rotator__case_active');
+//     }
+// }
+
+// let i = getIndex(ads);
+// setInterval(rotate(ads, i), getSpeed(ads, i))
 setInterval(rotate, 1000)
--- JS/bhj-homeworks-master/dom/book-reader/task.js
+++ JS/bhj-homeworks-master/dom/book-reader/task.js
@@ -2,13 +2,86 @@ const book = document.getElementById('book');
 
 const fontSizeSmallLink = document.querySelector('.font-size_small');
 const fontSizeBigLink = document.querySelector('.font-size_big');
+const fontSizeActiveLink = document.querySelector('.font-size_active');
 
 fontSizeSmallLink.addEventListener('click', event => {
   event.preventDefault();
   book.style.fontSize = 'small';
+  fontSizeActiveLink.classList.remove('font-size_active');
+  fontSizeBigLink.classList.remove('font-size_active');
+  fontSizeSmallLink.classList.add('font-size_active');
 });
 
 fontSizeBigLink.addEventListener('click', event => {
   event.preventDefault();
   book.style.fontSize = 'large';
+  fontSizeActiveLink.classList.remove('font-size_active');
+  fontSizeSmallLink.classList.remove('font-size_active');
+  fontSizeBigLink.classList.add('font-size_active');
+});
+
+fontSizeActiveLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.fontSize = 'medium';
+  fontSizeSmallLink.classList.remove('font-size_active');
+  fontSizeBigLink.classList.remove('font-size_active');
+  fontSizeActiveLink.classList.add('font-size_active');
+});
+
+// Повышенный уровень сложности
+
+const textBlackLink = document.querySelector('.text_color_black');
+const textGrayLink = document.querySelector('.text_color_gray');
+const textWhitesmokeLink = document.querySelector('.text_color_whitesmoke');
+
+textBlackLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.color = 'black';
+  textGrayLink.classList.remove('color_active');
+  textWhitesmokeLink.classList.remove('color_active');
+  textBlackLink.classList.add('color_active');
+});
+
+textGrayLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.color = 'gray';
+  textBlackLink.classList.remove('color_active');
+  textWhitesmokeLink.classList.remove('color_active');
+  textGrayLink.classList.add('color_active');
+});
+
+textWhitesmokeLink.addEventListener('click', event => {
+  event.preventDefault(); 
+  book.style.color = 'whitesmoke';
+  textBlackLink.classList.remove('color_active');
+  textGrayLink.classList.remove('color_active');
+  textWhitesmokeLink.classList.add('color_active');
+});
+
+const bgBlackLink = document.querySelector('.bg_color_black');
+const bgGrayLink = document.querySelector('.bg_color_gray');
+const bgWhiteLink = document.querySelector('.bg_color_white');
+
+bgBlackLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.backgroundColor = 'black';
+  bgGrayLink.classList.remove('color_active');
+  bgWhiteLink.classList.remove('color_active');
+  bgBlackLink.classList.add('color_active');
+});
+
+bgGrayLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.backgroundColor = 'gray';
+  bgBlackLink.classList.remove('color_active');
+  bgWhiteLink.classList.remove('color_active');
+  bgGrayLink.classList.add('color_active');
+});
+
+bgWhiteLink.addEventListener('click', event => {
+  event.preventDefault();
+  book.style.backgroundColor = 'white';
+  bgBlackLink.classList.remove('color_active');
+  bgGrayLink.classList.remove('color_active');
+  bgWhiteLink.classList.add('color_active');
 });
