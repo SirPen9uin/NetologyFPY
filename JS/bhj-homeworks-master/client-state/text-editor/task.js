fc79110da72d9aff4989a9c48328f07649cfb06a
Pen9uin <sirpen9uin@gmail.com>
2024-06-04 10:58:13 +0300

    text-editor extras

--- JS/bhj-homeworks-master/client-state/text-editor/task.js
+++ JS/bhj-homeworks-master/client-state/text-editor/task.js
@@ -1,5 +1,6 @@
 const textarea = document.querySelector('textarea');
 const storedText = localStorage.getItem('text');
+const btn = document.getElementById('clear');
 
 if (storedText) {
   textarea.value = storedText;
@@ -9,3 +10,8 @@ textarea.addEventListener('input', () => {
   const text = textarea.value;
   localStorage.setItem('text', text);
 });
+
+btn.addEventListener('click', () => {
+  textarea.value = '';
+  localStorage.removeItem('text');
+})
