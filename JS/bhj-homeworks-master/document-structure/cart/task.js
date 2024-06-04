d3b8d226d6a605525e18216b9f3c8a8c8da43c65
Pen9uin <sirpen9uin@gmail.com>
2024-06-04 10:37:48 +0300

    fixes(2)

--- JS/bhj-homeworks-master/document-structure/cart/task.js
+++ JS/bhj-homeworks-master/document-structure/cart/task.js
@@ -1,35 +1,42 @@
-const plus = document.getElementsByClassName('product__quantity-control product__quantity-control_inc')
-const minus = document.getElementsByClassName('product__quantity-control product__quantity-control_dec')
-const quantity = document.getElementsByClassName('product__quantity-value')
-const add = document.getElementsByClassName('product__add')
-const products = document.getElementsByClassName('product')
-const images = document.getElementsByClassName('product__image')
-const cart = document.querySelector('.cart__products')
-let arryId = []
-for (let i = 0; i< plus.length; i++){
-    plus[i].onclick = () => {
-    quantity[i].textContent = Number(quantity[i].textContent)+1
+const plus = document.getElementsByClassName('product__quantity-control product__quantity-control_inc');
+const minus = document.getElementsByClassName('product__quantity-control product__quantity-control_dec');
+const quantity = document.getElementsByClassName('product__quantity-value');
+const add = document.getElementsByClassName('product__add');
+const products = document.getElementsByClassName('product');
+const images = document.getElementsByClassName('product__image');
+const cart = document.querySelector('.cart__products');
+let arryId = [];
+
+for (let i = 0; i < plus.length; i++) {
+  plus[i].onclick = () => {
+    quantity[i].textContent = Number(quantity[i].textContent) + 1;
+  };
+
+  minus[i].onclick = () => {
+    if (quantity[i].textContent > 1) {
+      quantity[i].textContent = Number(quantity[i].textContent) - 1;
     }
-    minus[i].onclick = () => {
-        if (quantity[i].textContent > 1){
-            quantity[i].textContent = Number(quantity[i].textContent)-1
-        }
+  };
+
+  add[i].onclick = () => {
+    const productId = products[i].getAttribute('data-id');
+    const productInCart = Array.from(cart.querySelectorAll('.cart__product')).find(
+      (product) => product.getAttribute('data-id') === productId
+    );
+
+    if (productInCart) {
+      productInCart.querySelector('.cart__product-count').textContent =
+        Number(productInCart.querySelector('.cart__product-count').textContent) +
+        Number(quantity[i].textContent);
+    } else {
+      cart.innerHTML += `
+        <div class="cart__product" data-id="${productId}">
+          <img class="cart__product-image" src="${images[i].getAttribute('src')}">
+          <div class="cart__product-count">${quantity[i].textContent}</div>
+        </div>
+      `;
     }
-    add[i].onclick = () => {
-        const cartCount = document.querySelectorAll('.cart__product-count')
-        const productInCart = document.querySelectorAll('.cart__product')
-        console.log(products[i].getAttribute('data-id'))
-        
-        if (arryId.includes(products[i].getAttribute('data-id') ) ) {
-            cartCount[i].textContent = Number(cartCount[i].textContent) + Number(quantity[i].textContent)
-        }
-        else {
-        cart.innerHTML +=  
-       `<div class="cart__product" data-id="${products[i].getAttribute('data-id')}">
-           <img class="cart__product-image" src="${images[i].getAttribute('src')}">
-           <div class="cart__product-count">${quantity[i].textContent}</div>
-       </div>`
-        }
-        arryId.push(products[i].getAttribute('data-id'))
-    }
+
+    arryId.push(productId);
+  };
 }
--- JS/bhj-homeworks-master/document-structure/todo/task.js
+++ JS/bhj-homeworks-master/document-structure/todo/task.js
@@ -3,16 +3,20 @@ const inputEl = document.getElementById('task__input');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newEl = document.createElement('div');
-  newEl.classList.add('task');
-  newEl.innerHTML = `
-    <div class="task__title"></div>
-    <a href="#" class="task__remove">&times;</a>
-  `;
-  newEl.querySelector('.task__title').textContent = inputEl.value;
-  newEl.querySelector('.task__remove').addEventListener('click', () => {
-    newEl.remove();
-  });
-  document.getElementById('tasks__list').appendChild(newEl);
-  inputEl.value = '';
+  if (inputEl.value.trim() === '') {
+    return;
+  } else {
+    const newEl = document.createElement('div');
+    newEl.classList.add('task');
+    newEl.innerHTML = `
+      <div class="task__title"></div>
+      <a href="#" class="task__remove">&times;</a>
+    `;
+    newEl.querySelector('.task__title').textContent = inputEl.value;
+    newEl.querySelector('.task__remove').addEventListener('click', () => {
+      newEl.remove();
+    });
+    document.getElementById('tasks__list').appendChild(newEl);
+    inputEl.value = '';
+  }
 });
