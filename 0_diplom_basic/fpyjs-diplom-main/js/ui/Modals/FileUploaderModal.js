2c8c5f80d499e5cd06d8d2d8d54e5c7e6ad98e5e
Pen9uin <sirpen9uin@gmail.com>
2024-06-13 14:25:41 +0300

    first try

--- 0_diplom_basic/fpyjs-diplom-main/js/ui/Modals/FileUploaderModal.js
+++ 0_diplom_basic/fpyjs-diplom-main/js/ui/Modals/FileUploaderModal.js
@@ -4,7 +4,10 @@
  */
 class FileUploaderModal {
   constructor( element ) {
-
+    super(element);
+    this.element = document.querySelector('.file-uploader-modal');
+    this.content = this.element.querySelector('.content');
+    this.registerEvents();
   }
 
   /**
@@ -17,34 +20,93 @@ class FileUploaderModal {
    * отправляет одно изображение, если клик был по кнопке отправки
    */
   registerEvents(){
+    this.element.addEventListener('click', (event) => {
+      if (event.target.classList.contains('x')) {
+        this.close();
+      }
 
+      if (event.target.classList.contains('close')) {
+        this.close();
+      }
+
+      if (event.target.classList.contains('send-all')) {
+        this.sendAllImages();
+      }
+
+      if (event.target.contains('common-path')) {
+        let commonPath = prompt('Введите путь в облаке: ');
+        [...this.content.querySelectorAll('.image-preview-container')].forEach(el => {
+          el.querySelector('input').value = commonPath;
+        });
+      }
+  });
+
+  this.content.addEventListener('click', (event) => {
+    if (event.target.tagName == 'input') {
+      event.target.closest('.input').classList.remove('error');
+    }
+
+    if (event.target.classList.contains('button')) {
+      this.sendImage(event.target.closest('.image-preview-container'));
+    }
+  });
   }
 
   /**
    * Отображает все полученные изображения в теле всплывающего окна
    */
   showImages(images) {
-
+    let imagesRevers = images.reverse();
+    let imagesModal = [];
+    imagesRevers.forEach(el => imagesModal.push(this.getImageHTML(el)));
+    this.content.innerHTML = imagesModal.join('');
   }
 
   /**
    * Формирует HTML разметку с изображением, полем ввода для имени файла и кнопкной загрузки
    */
   getImageHTML(item) {
-
+    return `
+    <div class="image-preview-container">
+      <img src=${item.querySelector('img').getAttribute('src')} />
+      <div class="ui action input">
+        <input type="text" placeholder="Путь к файлу">
+        <button class="ui button"><i class="upload icon"></i></button>
+      </div>
+    </div>
+    `
   }
 
   /**
    * Отправляет все изображения в облако
    */
   sendAllImages() {
-
+    [...this.content.querySelectorAll('.image-preview-container')].forEach(el => {
+      this.sendImage(el);
+    })
   }
 
   /**
    * Валидирует изображение и отправляет его на сервер
    */
   sendImage(imageContainer) {
-
+    let url = imageContainer.querySelector('img').getAttribute('src');
+    let path = imageContainer.querySelector('input').value.trim();
+    if (path == '') {
+      imageContainer.querySelector('.input').classList.add('error');
+      return;
+    };
+    imageContainer.querySelector('.input').classList.add('disabled');
+    Yandex.uploadFile(path, url, (err) => {
+      if (err === false) {
+        imageContainer.querySelector('.input').classList.remove('disabled');
+        imageContainer.querySelector('input').value = '';
+      } else {
+        imageContainer.remove();
+        if ([...this.content.querySelectorAll('.image-preview-container')].length < 1) {
+          this.close();
+      }
+    }
+  });
   }
 }
--- 0_diplom_basic/fpyjs-diplom-main/js/ui/Modals/PreviewModal.js
+++ 0_diplom_basic/fpyjs-diplom-main/js/ui/Modals/PreviewModal.js
@@ -4,7 +4,10 @@
  */
 class PreviewModal {
   constructor( element ) {
-
+    super(element);
+    this.element = document.querySelector('.uploaded-previewer-modal');
+    this.content = this.element.querySelector('.content');
+    this.registerEvents();
   }
 
   /**
@@ -15,6 +18,30 @@ class PreviewModal {
    * Скачивает изображение, если клик был на кнопке download
    */
   registerEvents() {
+    this.element.addEventListener('click', (event) => {
+      
+      if (event.target.classList.contains('x')) {
+        this.close();
+      }
+    });
+
+    this.content.addEventListener('click', (event) => {
+
+      if (event.target.classList.contains('delete')) {
+        event.target.querySelector('i').className = 'icon spinner loading';
+        event.target.classList.add('disabled');
+        const path = event.target.dataset.version != 'undefined' ? `${event.target.dataset.path} ${event.target.dataset.version}` : event.target.dataset.path;
+        Yandex.removeFile(path, (resp) => {
+          if (typeof(resp) === 'object') {
+            event.target.closest('.image-preview-container').remove();
+          }
+        });
+    }
+
+    if (event.target.classList.contains('download')) {
+      Yandex.downloadFileByUrl(event.target.dataset.file);
+    }
+  })
 
   }
 
@@ -23,7 +50,10 @@ class PreviewModal {
    * Отрисовывает изображения в блоке всплывающего окна
    */
   showImages(data) {
-
+    let dataRevers = data['items'].reverse().filter(el => el['media_type'] == 'image');
+    let imagesList = [];
+    dataRevers.forEach(el => imagesList.push(this.getImageInfo(el)));
+    this.content.innerHTML = imagesList.join('');
   }
 
   /**
@@ -31,13 +61,42 @@ class PreviewModal {
    * в формат «30 декабря 2021 г. в 23:40» (учитывая временной пояс)
    * */
   formatDate(date) {
-
+    const newDate = new Date (date);
+    let day = newDate.getDate();
+    let month = new Intl.DateTimeFormat("ru-RU", {month: "long"}).format(newDate.getMonth()).toLowerCase();
+    let year = newDate.getFullYear();
+    let hours = newDate.getHours();
+    let minutes = newDate.getMinutes();
+    return `${day} ${month} ${year} в ${hours}:${minutes}`
   }
 
   /**
    * Возвращает разметку из изображения, таблицы с описанием данных изображения и кнопок контроллеров (удаления и скачивания)
    */
   getImageInfo(item) {
-
+    let version = item['path'].match(/\(.+\)/gm) ? item['path'].match(/\(.+\)/gm)[0]: undefined;
+    return `
+    <div class="image-preview-container">
+        <img src=${item['preview']} />
+        <table class="ui celled table">
+        <thead>
+          <tr><th>Имя</th><th>Создано</th><th>Размер</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>${item['name']}</td><td>${this.formatDate(item['created'])}</td><td>${item['size']} Кб</td></tr>
+        </tbody>
+        </table>
+        <div class="buttons-wrapper">
+          <button class="ui labeled icon red basic button delete" data-path=${item['path']} data-version=${version}>
+            Удалить
+            <i class="trash icon"></i>
+          </button>
+          <button class="ui labeled icon violet basic button download" data-file=${item['file']}>
+            Скачать
+            <i class="download icon"></i>
+          </button>
+        </div>
+      </div>
+    `
   }
 }
